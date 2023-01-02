a1b98fb252713d702d322201901eed5275da273c
Jacky <jackypengsmail@gmail.com>
2023-01-01 23:34:59 -0500

    Implemented user identification

--- middleware/userAuth.js
+++ middleware/userAuth.js
@@ -4,24 +4,31 @@ const User = require("../models/user")
 const userAuth = (req, res, next) => {
 
     const { authorization } = req.headers
+    console.log(req.body)
 
     if (!authorization) {
         return res.status(401).json({ error: "No authorization token" })
     }
 
     const token = authorization.split(" ")[1]
-    console.log(jwt.verify(token, process.env.SECRET))
+    // console.log(jwt.verify(token, process.env.SECRET))
 
 
     try {
         const {id} = jwt.verify(token, process.env.SECRET)
-        console.log(id)
-        User.findOne({ id })
-            .then(result => {
-                req.user = result._id
-                console.log(req.user)
-                next()
-            })
+        // console.log(id)
+        // User.findOne({ id }).select("id")
+        //     .then(result => {
+        //         console.log(result, "RESULTTTTTTTTT")
+        //         req.user = result.id
+        //         console.log(req.user, "REQUSER")
+        //         next()
+        //     })
+        User.findOne({"_id" : id}).select("_id")
+        .then(result => {
+            req.user = result.id
+            next()
+        })
     } catch {
         console.log("catch")
         res.status(401).json({ mssg: "Request is not authorized" })
